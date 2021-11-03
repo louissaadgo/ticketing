fb09704935a94c0645e8c91d3aa127522b33c8e4
louissaadgo <louissaadgo@gmail.com>
2021-11-03 14:38:15 +0200

    adde get handler for authservice

--- auth/main.go
+++ auth/main.go
@@ -5,5 +5,9 @@ import "github.com/gofiber/fiber/v2"
 func main() {
 	app := fiber.New()
 
+	app.Get("/api/users/currentuser", func(c *fiber.Ctx) error {
+		return c.SendString("Hi there")
+	})
+
 	app.Listen(":3000")
 }
