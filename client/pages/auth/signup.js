5761be3aa11737193b7ea9bbb23e253b6c4928d0
louissaadgo <louissaadgo@gmail.com>
2021-11-10 14:53:00 +0200

    Implemented auth/signup && /

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,22 @@
 import { useState } from "react";
-import axios from "axios";
+import Router from 'next/router';
+import useRequest from "../../hooks/use-request";
 
 export default () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState([]);
+    const { doRequest, errors} = useRequest({
+        url: '/api/users/signup',
+        method: 'post',
+        body: {
+            email, password
+        },
+        onSuccess: () => Router.push('/')
+    });
     const onSubmit = async (event) => {
         event.preventDefault();
-        
-        try {
-        const response = await axios.post('https://ticketing.dev/api/users/signup', {
-            email, password
-        })
 
-        console.log(response)
-    } catch (err){
-        setErrors(err.response.data.errors)
-    }
+        await doRequest();
     };
 
     return <form onSubmit={onSubmit}>
@@ -29,12 +29,7 @@ export default () => {
             <label>Password</label>
             <input value={password} onChange={e => setPassword(e.target.value)} type="password" className="form-control" />
         </div>
-        {errors.length > 0 && <div className="alert alert-danger">
-            <h4>Ooops...</h4>
-            <ul className="my-0">
-        {errors.map(err => <li key={err.message}>{err.message}</li>)}
-        </ul>
-        </div>}
+        {errors}
         <button className="btn btn-primary">Sign Up</button>
     </form>
 };
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,12 @@
-export default () => {
-  return <h1>Hello</h1>
-}
+import buildClient from "../api/build-client";
+
+const LandingPage = ({currentUser}) => {
+  return <h1>{currentUser}</h1>
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const {data} = await buildClient(context).get('/api/users/currentuser')
+  return data
+};
+
+export default LandingPage;
