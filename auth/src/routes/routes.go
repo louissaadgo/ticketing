389ffcdc88bad6a4abfa8e534dbe4386ee30063a
louissaadgo <louissaadgo@gmail.com>
2021-11-10 21:11:26 +0200

    Removed CORS

--- auth/src/routes/routes.go
+++ auth/src/routes/routes.go
@@ -2,14 +2,12 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/louissaadgo/ticketing/auth/src/controllers"
 	"github.com/louissaadgo/ticketing/auth/src/middlewares"
 )
 
 func Init(app *fiber.App) {
 
-	app.Use(cors.New())
 	app.Post("/api/users/signin", controllers.Signin)
 	app.Post("/api/users/signup", controllers.Signup)
 	app.Post("/api/users/signout", controllers.Signout)
