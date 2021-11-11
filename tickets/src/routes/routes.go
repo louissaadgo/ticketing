59a6c858ee6bb9572de8c7191ce7e69404fae002
louissaadgo <louissaadgo@gmail.com>
2021-11-11 18:57:14 +0200

    Added token middleware

--- tickets/src/routes/routes.go
+++ tickets/src/routes/routes.go
@@ -3,10 +3,12 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/louissaadgo/ticketing/tickets/src/controllers"
+	"github.com/louissaadgo/ticketing/tickets/src/middlewares"
 )
 
 func Init(app *fiber.App) {
 
+	app.Use(middlewares.IsValidPasetoToken)
 	app.Post("/api/tickets", controllers.CreateTicket)
 
 }
