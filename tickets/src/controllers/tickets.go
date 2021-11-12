63cbe3665b25d923a17ecf4c1c97c68bb72bb541
louissaadgo <louissaadgo@gmail.com>
2021-11-12 12:00:01 +0200

    Added error models and ticketcreation flow

--- tickets/src/controllers/tickets.go
+++ tickets/src/controllers/tickets.go
@@ -1,7 +1,40 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"context"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/louissaadgo/ticketing/tickets/src/database"
+	"github.com/louissaadgo/ticketing/tickets/src/models"
+)
 
 func CreateTicket(c *fiber.Ctx) error {
-	return c.SendString("Hello")
+
+	ticket := models.Ticket{}
+
+	//Validating received data type
+	if err := c.BodyParser(&ticket); err != nil {
+		validationError := models.Error{}
+		validationError.Message = "Received invalid data type"
+		errorResponse := models.ErrorResponse{}
+		errorResponse.Errors = append(errorResponse.Errors, validationError)
+		c.Status(400)
+		return c.JSON(errorResponse)
+	}
+
+	//Validating received data
+	if isValid := ticket.Validate(); !isValid {
+		validationError := models.Error{}
+		validationError.Message = "Received invalid data"
+		errorResponse := models.ErrorResponse{}
+		errorResponse.Errors = append(errorResponse.Errors, validationError)
+		c.Status(400)
+		return c.JSON(errorResponse)
+	}
+
+	//Inserting ticket into DB
+	//Handle db error later
+	database.DB.InsertOne(context.TODO(), ticket)
+
+	return c.JSON(ticket)
 }
