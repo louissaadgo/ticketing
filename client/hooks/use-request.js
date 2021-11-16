b38fd6da97e17f56032e8ec683e097dac33e0d5c
louissaadgo <louissaadgo@gmail.com>
2021-11-16 13:10:09 +0200

    removed useless code

--- client/hooks/use-request.js
+++ client/hooks/use-request.js
@@ -27,25 +27,6 @@ export default ({url, method, body, onSuccess}) => {
                     </div>
                 );
             });
-        // try{
-        //     setErrors(null);
-        //     const response = await axios[method](url,body);
-            // if (onSuccess) {
-            //     onSuccess(response.data)
-            // }
-            // console.log(response)
-            // return response.data;
-        // }catch (err) {
-        //     console.log(err)
-        //     setErrors(
-        //         <div className="alert alert-danger">
-        //     <h4>Ooops...</h4>
-        //     <ul className="my-0">
-        // {err.response.data.errors.map(err => <li key={err.message}>{err.message}</li>)}
-        // </ul>
-        // </div>
-        //     );
-        // }
     };
 
     return { doRequest, errors};
