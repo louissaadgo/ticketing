8ce3f07e7e2380dc977d38e35398dc3a3d12184c
louissaadgo <louissaadgo@gmail.com>
2021-11-18 17:37:13 +0200

    change email adress field

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -22,7 +22,7 @@ export default () => {
     return <form onSubmit={onSubmit}>
         <h1>Signup</h1>
         <div className="form-group">
-            <label>Email Adress</label>
+            <label>Email</label>
             <input value={email} onChange={e => setEmail(e.target.value)} className="form-control" />
         </div>
         <div className="form-group">
