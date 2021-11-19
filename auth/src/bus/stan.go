259d45fc739c9c5291ada6f20590a13d3e85d8ac
louissaadgo <louissaadgo@gmail.com>
2021-11-19 23:06:40 +0200

    added stan controller

---
+++ auth/src/bus/stan.go
@@ -0,0 +1,27 @@
+package bus
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/nats-io/stan.go"
+)
+
+var SC stan.Conn
+
+func CreateSTANConnection(id string) {
+	SC, _ = stan.Connect("ticketing", id, stan.NatsURL("http://localhost:4222"))
+	//handle error later
+}
+
+func CreateSTANListener(subject string, q string, fun func(m *stan.Msg)) {
+	_, err := SC.QueueSubscribe(subject, q, fun, stan.DeliverAllAvailable(), stan.SetManualAckMode(), stan.DurableName(q), stan.AckWait(5*time.Second))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func STANPublish(subject string, data []byte) {
+	SC.Publish(subject, data)
+	//handle error later
+}
