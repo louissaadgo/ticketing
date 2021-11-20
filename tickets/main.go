234e859f288e92f4d91e57559f26121b8386399e
louissaadgo <louissaadgo@gmail.com>
2021-11-20 13:15:02 +0200

    Configured stan

--- tickets/main.go
+++ tickets/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/louissaadgo/ticketing/tickets/src/bus"
 	"github.com/louissaadgo/ticketing/tickets/src/database"
 	"github.com/louissaadgo/ticketing/tickets/src/routes"
 )
 
 func main() {
 	//Add testing later
+	bus.CreateSTANConnection()
 
 	app := fiber.New()
 
