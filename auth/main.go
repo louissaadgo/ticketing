1d3a3157dedc906512d2fc6afb1156aff27932f2
louissaadgo <louissaadgo@gmail.com>
2021-11-20 12:32:00 +0200

    refactoring

--- auth/main.go
+++ auth/main.go
@@ -1,23 +1,15 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/louissaadgo/ticketing/auth/src/bus"
 	"github.com/louissaadgo/ticketing/auth/src/database"
 	"github.com/louissaadgo/ticketing/auth/src/routes"
-	"github.com/nats-io/stan.go"
 )
 
 func main() {
 	//Add testing later
 	bus.CreateSTANConnection()
-
-	go bus.CreateSTANListener("auth:created-user", "auth-service", func(m *stan.Msg) {
-		fmt.Printf("New message: %v", m.Data)
-		m.Ack()
-	})
 
 	app := fiber.New()
 
